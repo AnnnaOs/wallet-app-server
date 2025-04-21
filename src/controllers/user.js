79e33436c6ee6343d35ebdb1b5bde987f83f6f29
diana_dobosh <diana.lakatosh.girl@gmail.com>
2025-04-21 13:20:51 +0300

    added patch to update the user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,17 +1,29 @@
-import createHttpError from "http-errors";
-import { getCurrentUser } from "../services/user.js"
-
+import createHttpError from 'http-errors';
+import { getCurrentUser, updateUser } from '../services/user.js';
 
 export const getCurrentUserController = async (req, res, next) => {
-    const userId = req.user._id;
-    const currentUser = await getCurrentUser(userId);
-    if(!currentUser) {
-        throw createHttpError(404, 'User not found');
-    }
-    res.status(200).json({
-        status: 200,
-        message: `Successfully found user with id: ${userId}!`,
-        data: currentUser,
-    });
+  const userId = req.user._id;
+  const currentUser = await getCurrentUser(userId);
+  if (!currentUser) {
+    throw createHttpError(404, 'User not found');
+  }
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found user with id: ${userId}!`,
+    data: currentUser,
+  });
+};
 
-}
+export const patchUserController = async (req, res) => {
+  const { userId } = req.user._id;
+  const result = await updateUser(userId, req.body);
+  if (!result) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a user!`,
+    data: result.user,
+  });
+};
