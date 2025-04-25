1ed90cc4919c58008882566782fcfe0162ec5ce3
Vladyslav Svintsitskyi <svintsitskyi1999@gmail.com>
2025-04-25 20:15:20 +0200

    refactor of auth service and auth controller

---
+++ src/utils/auth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import { getEnvVar } from './getEnvVar.js';
+import { SessionsCollection } from '../db/models/session.js';
+
+const JWT_SECRET = getEnvVar('JWT_SECRET');
+const ACCESS_TOKEN_EXPIRES = '24h';
+
+export const createSessionForUser = async (user) => {
+  const accessToken = jwt.sign({ userId: user._id }, JWT_SECRET, {
+    expiresIn: ACCESS_TOKEN_EXPIRES,
+  });
+
+  const session = await SessionsCollection.create({
+    userId: user._id,
+    accessToken,
+    accessTokenValidUntil: new Date(Date.now() + 3600000),
+  });
+
+  return { accessToken, sessionId: session._id };
+};
