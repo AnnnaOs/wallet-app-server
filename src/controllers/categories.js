ba94e170e410b6f60f4ca898ed719b6fdffef562
Vladyslav Svintsitskyi <svintsitskyi1999@gmail.com>
2025-05-01 13:28:58 +0200

    fix fetch categories for summary and for modals

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,35 +1,41 @@
 import { TransactionsCollection } from '../db/models/transaction.js';
 
-export const getTransactionCategories = async (req, res) => {
+export const getTransactionCategories = async (req, res, next) => {
   try {
-    const userId = req.user._id;
+    // Логируем путь к категории
+    const categoryPath = TransactionsCollection.schema.path('category');
+    if (!categoryPath) {
+      console.error('categoryPath is undefined');
+      throw new Error('categoryPath is undefined');
+    }
 
-    const incomeCategories = await TransactionsCollection.distinct('category', {
-      userId,
-      type: 'Income',
-    });
+    const categories = categoryPath.enumValues;
+    console.log('All categories:', categories);
 
-    const expenseCategories = await TransactionsCollection.distinct(
-      'category',
-      {
-        userId,
-        type: 'Expense',
-      },
-    );
+    // Фильтруем категории по типу
+    const income = categories.filter((c) => c === 'Income');
+    const expenses = categories.filter((c) => c !== 'Income');
 
-    const categories = {
-      income: incomeCategories,
-      expense: expenseCategories,
-    };
+    console.log('Income categories:', income);
+    console.log('Expense categories:', expenses);
 
-    res.status(200).json({
-      status: 200,
-      data: categories,
-    });
+    // Получаем тип из query параметра
+    const { type } = req.query;
+
+    // Проверяем на тип в запросе
+    if (type === 'Income') {
+      console.log('Returning income categories');
+      return res.status(200).json({ income });
+    } else if (type === 'Expense') {
+      console.log('Returning expense categories');
+      return res.status(200).json({ expenses });
+    }
+
+    // Если тип не передан, возвращаем все категории
+    console.log('Returning all categories');
+    res.status(200).json({ income, expenses });
   } catch (error) {
-    console.error("Can't get transaction categories:", error.message);
-    res.status(500).json({
-      message: error.message || 'Server error',
-    });
+    console.error('Error in getCategoriesController:', error.message);
+    next(error); // передаем ошибку дальше, чтобы сервер не упал
   }
 };
